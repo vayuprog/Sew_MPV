7994ca8278826a79ab186d41535bc7703e1ba7bb
vayuprog <vaniayushchuk.15@gmail.com>
2024-02-10 17:06:50 +0200

    first commit

---
+++ src/main/java/dev/sew/sew/SewMvpApplication.java
@@ -0,0 +1,13 @@
+package dev.sew.sew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SewMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sew/sew/SewMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sew.sew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SewMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
